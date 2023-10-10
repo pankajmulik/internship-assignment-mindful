99740cdf53798ca361fb5fea648881d952b51eb0
pankajmulik <pankajmulik1997@gmail.com>
2023-10-10 14:30:45 +0530

    commit backend

---
+++ backend/model/newUserModel.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const newUserModel = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.Schema("User", newUserModel);
+
+module.exports = User;
