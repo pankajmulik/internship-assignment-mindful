076aa00a232a1418d750fedd4130969e3f73144b
pankajmulik <pankajmulik1997@gmail.com>
2023-10-11 22:26:01 +0530

    git push

--- backend/controller/workoutController.js
+++ backend/controller/workoutController.js
@@ -1,9 +1,13 @@
+const express = require("express");
 
-const express = require("express")
+const mongoose = require("mongoose");
 
-const mongoose = require("mongoose")
+const addUser = require("../model/addUser");
 
-const getAdedUser = await('/', (req, res) => {
-    const
-})
+const getAddedUser = async (req, res) => {
+  const users = await addUser.find({}).sort({ createdAt: -1 });
 
+  res.status(200).json(users);
+};
+
+module.exports = getAddedUser;
