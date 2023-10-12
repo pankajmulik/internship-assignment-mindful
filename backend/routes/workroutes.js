d8c0629dd8d7f0491c7671b8cf3e591dd6f9911f
PANKAJ MULIK <pankajmulik1997@gmail.com>
2023-10-12 12:04:19 +0530

    Update workroutes.js

--- backend/routes/workroutes.js
+++ backend/routes/workroutes.js
@@ -8,10 +8,19 @@ const {
   deleteUser,
 } = require("../controller/workoutController");
 
+
+
+const { getUser, createNewUser } = require("../controller/signupController");
+
 router.use("/", getAddedUser);
 
 router.use("/create", createUser);
 
 router.use("/delete", deleteUser);
+
+router.use("/update", updateUser);
+
+router.use("/signup", createNewUser);
+router.use("/profile", getUser);
 
 module.exports = router;
