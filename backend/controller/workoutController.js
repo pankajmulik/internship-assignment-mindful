8ce1a47e3d57b1df698ecce0396f2f6a494632eb
PANKAJ MULIK <pankajmulik1997@gmail.com>
2023-10-12 13:06:40 +0530

    Update workoutController.js

--- backend/controller/workoutController.js
+++ backend/controller/workoutController.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const Workout = require("../model/addUser");
+const newWorkout = require("../model/newUserModel");
 
 const getAddedUser = async (req, res) => {
   const users = await Workout.find({}).sort({ createdAt: -1 });
@@ -36,4 +37,62 @@ const deleteUser = async (req, res) => {
   res.status(200).json(user);
 };
 
-module.exports = { getAddedUser, createUser, deleteUser };
+const updateUser = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "no such workout" });
+  }
+  const user = await Workout.findOneAndUpdate(
+    { _id: id },
+    {
+      ...req.body,
+    }
+  );
+
+  if (!user) {
+    return res.status(400).json({ error: "no such workout" });
+  }
+  res.status(200).json(user);
+};
+
+const userProfile = async (req, res) => {
+  const { id } = req.params;
+  const user = await newWorkout.findById(id);
+
+  if (!user) {
+    return res.status(404).json({ error: "no such workout" });
+  }
+
+  res.status(200).json(user);
+};
+
+const signupUser = async (req, res) => {
+  const { name, email, phone, address, gender, referal, city, state } =
+    req.body;
+
+  try {
+    const user = await newWorkout.create({
+      name,
+      email,
+      phone,
+      address,
+      gender,
+      referal,
+      city,
+      state,
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  getAddedUser,
+  createUser,
+  deleteUser,
+  updateUser,
+  userProfile,
+  signupUser,
+};
