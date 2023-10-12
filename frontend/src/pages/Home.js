009b829044d4b4703ecf496a7aa7bfbe34bcfe4d
PANKAJ MULIK <pankajmulik1997@gmail.com>
2023-10-12 21:12:11 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup"
 
 const Home = () => {
+const [formAction, setformActon]=useState("login")
+
+  
+  
   return (
-    <div>
-      <Login />
+
+      <div className="Home">
+<div className="form-buttons">
+      <button onClick={()=>setformActon("login")}>Login</button>
+   <button onClick={()=>setformActon("signup")}>Signup</button>
+      
+      </div>
+      <div>
+      {
+          formAction === "login" ? <Login /> : "signup" 
+          
+        
+      }
+      {
+        formAction === "signup" ? <Signup/> : "login"
+      }
+      
+      
+
+      </div>
+
     </div>
   );
 };
